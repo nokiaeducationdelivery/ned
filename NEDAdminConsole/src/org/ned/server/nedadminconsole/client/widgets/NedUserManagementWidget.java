18e21b9d0b833ecd28db6e69891dfe9e962d5e46
Dominik Frankowski <dominik.frankowski@comarch.com>
2011-12-19 09:56:56 +0100

    adding Eclipse license header to java files

--- NEDAdminConsole/src/org/ned/server/nedadminconsole/client/widgets/NedUserManagementWidget.java
+++ NEDAdminConsole/src/org/ned/server/nedadminconsole/client/widgets/NedUserManagementWidget.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+* Copyright (c) 2011 Nokia Corporation
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* Comarch team - initial API and implementation
+*******************************************************************************/
 package org.ned.server.nedadminconsole.client.widgets;
 
 import java.util.LinkedList;
@@ -27,7 +37,6 @@ import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
--- NEDAdminConsole/src/org/ned/server/nedadminconsole/server/NedStatisticsFileServlet.java
+++ NEDAdminConsole/src/org/ned/server/nedadminconsole/server/NedStatisticsFileServlet.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+* Copyright (c) 2011 Nokia Corporation
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* Comarch team - initial API and implementation
+*******************************************************************************/
 package org.ned.server.nedadminconsole.server;
 
 import java.io.IOException;
